eec2705113635a9ae04fc46669bc5e0637a7fd16
Stan Tarnovskiy <starnovsky@gmail.com>
2016-05-06 12:25:00 -0700

    uncommented, installed

--- 5 - Test/KC_Test/test/postalCodeValidatorTests_qunit.js
+++ 5 - Test/KC_Test/test/postalCodeValidatorTests_qunit.js
@@ -9,29 +9,29 @@ QUnit.test('expect 12345 to be a valid US postal code', function (assert) {
 	assert.ok(isValid === true, 'expected 12345 to be a valid US postal code');
 });
 
-//QUnit.test('expect undefined to be an invalid postal code', function (assert) {
+QUnit.test('expect undefined to be an invalid postal code', function (assert) {
 
-//	this.validator = new PostalCodeValidator();
+	this.validator = new PostalCodeValidator();
 
-//	var isValid = this.validator.isValid(undefined);
+	var isValid = this.validator.isValid(undefined);
 
-//	assert.ok(isValid === false, 'expected undefined to be an invalid postal code');
-//});
+	assert.ok(isValid === false, 'expected undefined to be an invalid postal code');
+});
 
-//QUnit.test('expect 123 to be an invalid postal code', function (assert) {
+QUnit.test('expect 123 to be an invalid postal code', function (assert) {
 
-//	this.validator = new PostalCodeValidator();
+	this.validator = new PostalCodeValidator();
 
-//	var isValid = this.validator.isValid('123');
+	var isValid = this.validator.isValid('123');
 
-//	assert.ok(isValid === false, 'Expected 123 to be an invalid postal code');
-//});
+	assert.ok(isValid === false, 'Expected 123 to be an invalid postal code');
+});
 
-//QUnit.test('expect AA11 1AA to be a valid postal code', function (assert) {
+QUnit.test('expect AA11 1AA to be a valid postal code', function (assert) {
 
-//	this.validator = new PostalCodeValidator();
+	this.validator = new PostalCodeValidator();
 
-//	var isValid = this.validator.isValid('AA11 1AA');
+	var isValid = this.validator.isValid('AA11 1AA');
 
-//	assert.ok(isValid === true, 'Expected AA11 1AA to be a valid postal code');
-//});
+	assert.ok(isValid === true, 'Expected AA11 1AA to be a valid postal code');
+});
